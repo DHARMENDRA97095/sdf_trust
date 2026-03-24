3e330535b88bd3a1d8e9e30ee9249b7e3585ab6e
Dharmendra Prajapati <dharmendra97095@gmail.com>
2026-03-24 17:15:41 +0530

    All code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,25 @@
+import { API_BASE_URL } from "../config";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 // vite.config.js
+
+export const PROJECTS_API = `${API_BASE_URL}`;
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/admin': {
-        target: 'http://localhost/sdftrust/backend/admin',
+        target: `${API_BASE_URL}/backend/admin`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/admin/, ''),
       },
       // ADD THIS so images show up in your React frontend
       '/uploads': {
-        target: 'http://localhost/sdftrust/backend/admin/uploads',
+        target: `${API_BASE_URL}/admin/uploads`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/uploads/, ''),
       },
